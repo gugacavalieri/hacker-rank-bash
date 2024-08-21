26c57b1d30cdab5371d7fdb58ce03ea465e3ef0a
Guga Cavalieri <gugacavalieri@gmail.com>
2024-08-21 14:45:38 -0600

    Update Test Name

--- problems/lets-echo/problem.test.sh
+++ problems/lets-echo/problem.test.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+SCRIPT_NAME="./problems/lets-echo/problem.sh"
+
 setup() {
     bats_load_library bats-support
     bats_load_library bats-assert
     echo "$PWD"
 }
 
-@test "can run our script" {
-    run  "./problems/lets-echo/problem.sh"
+@test "$SCRIPT_NAME: can run our script" {
+    run  "$SCRIPT_NAME"
 }
 
-@test "it should output HELLO" {
-    run "./problems/lets-echo/problem.sh"
+@test "$SCRIPT_NAME: it should output HELLO" {
+    run "$SCRIPT_NAME"
     assert_output --partial 'HELLO'
 }
